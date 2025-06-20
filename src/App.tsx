9e8cd9d1dc70eedab07449e0a0294fd42fea178c
ankit-singh100 <asrankit456@gmail.com>
2025-06-20 10:13:58 +0545

    "create header component"

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,12 @@ import Signin from "./Pages/Signin";
 import SignUp from "./Pages/SignUp";
 import About from "./Pages/About";
 import Profile from "./Pages/Profile";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         |<Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<Signin />} />
