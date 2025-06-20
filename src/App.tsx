5ff49c2ec971098b3e1bf48f57545c8828127085
ankit-singh100 <asrankit456@gmail.com>
2025-06-20 08:48:28 +0545

    Install tailwindcss and react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 // import React from 'react'
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Signin from "./Pages/Signin";
+import SignUp from "./Pages/SignUp";
+import About from "./Pages/About";
+import Profile from "./Pages/Profile";
+
 function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        |<Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
